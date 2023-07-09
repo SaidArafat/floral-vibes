8a98731cca9dc1615885c1edc0458e49c320c6c2
Said Arafat <s.arafat.selmi@gmail.com>
2023-07-09 17:08:50 +0300

    Add testimonials section

---
+++ src/components/testimonials.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import StatusIndicator from "./common/statusIndicator";
+import Slider from "./common/slider";
+import Callout from "./callout";
+import { fetchFeedbacks } from "../store/slices/feedbackSlice";
+
+const Testimonials = () => {
+  const { feedbacks, isLoading, error } = useSelector(
+    (state) => state.feedbacks
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchFeedbacks());
+  }, [dispatch]);
+
+  return (
+    <StatusIndicator isLoading={isLoading} error={error}>
+      <section className="p-8 bg-white pb-16">
+        <h2 className="text-3xl mb-4">Testimonials</h2>
+        <Slider slides={feedbacks} testimonials />
+        <Callout />
+      </section>
+    </StatusIndicator>
+  );
+};
+
+export default Testimonials;
