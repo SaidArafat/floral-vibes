9d1051060a29fe167bc82e5e2366eba826e55196
Said Arafat <s.arafat.selmi@gmail.com>
2023-07-09 17:07:48 +0300

    Add bestseller section

---
+++ src/components/bestSellers.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import Slider from "./common/slider";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFlowers } from "./../store/slices/flowerSlice";
+import StatusIndicator from "./common/statusIndicator";
+
+const BestSellers = () => {
+  const { flowers, isLoading, error } = useSelector((state) => state.flowers);
+  // const { isSuccess } = useSelector((state) => state.cart);
+  // console.log(isSuccess);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchFlowers());
+  }, [dispatch]);
+
+  const bestSeller = flowers.filter((flower) => {
+    return flower.isBestseller === true;
+  });
+
+  return (
+    <StatusIndicator isLoading={isLoading} error={error}>
+      <section className="bg-white p-8">
+        <h2 className="text-3xl mb-4 capitalize">Our Best Sellers</h2>
+        <Slider slides={bestSeller} />
+      </section>
+    </StatusIndicator>
+  );
+};
+
+export default BestSellers;
